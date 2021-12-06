b7efdf403045ec57e2d6f0fa624b37e22d532b99
benjaminlimjt <benjaminlimjt@gmail.com>
2021-12-07 00:48:51 +0800

    Revert "Dashboard page change"
    This reverts commit 5579c40b7aaab619ce38fb6982f31c0ba402c802.

--- venv/static/js/scripts.js
+++ venv/static/js/scripts.js
@@ -43,39 +43,25 @@ $("form[name=createUser_form").submit(function (e) {
 
 }
 
-function validate() {
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("passwd").value;
-            if (username == null || username == "") {
-                alert("Please enter the username.");
-                return false;
-            }
-            if (password == null || password == "") {
-                alert("Please enter the password.");
-                return false;
-            }
-            
-    $("form[name=login_form").submit(function (e) {
+$("form[name=login_form").submit(function (e) {
 
-        var $form = $(this);
-        var $error = $form.find(".error");
-        var data = $form.serialize();
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
 
-        $.ajax({
-            url: "/login",
-            type: "POST",
-            data: data,
-            dataType: "json",
-            success: function (resp) {
-                console.log(resp);
-            },
-            error: function (resp) {
-                console.log(resp);
-            }
+    $.ajax({
+        url: "/login",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function (resp) {
+            console.log(resp);
+        },
+        error: function(resp) {
+            console.log(resp);
+        }
 
-        })
-
-        e.preventDefault();
     })
-                    
-}
+
+    e.preventDefault();
+})
